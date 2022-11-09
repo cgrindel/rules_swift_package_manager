964d2e929dfd7a0a04722bc03c0ebae21881774c
Chuck Grindel <chuck@chuckgrindel.com>
2022-11-09 16:14:05 -0700

    Add initial Language files

---
+++ gazelle/lang.go
@@ -0,0 +1,70 @@
+package gazelle
+
+import (
+	"path"
+
+	"github.com/bazelbuild/bazel-gazelle/config"
+	"github.com/bazelbuild/bazel-gazelle/label"
+	"github.com/bazelbuild/bazel-gazelle/language"
+	"github.com/bazelbuild/bazel-gazelle/repo"
+	"github.com/bazelbuild/bazel-gazelle/resolve"
+	"github.com/bazelbuild/bazel-gazelle/rule"
+)
+
+const swiftName = "swift"
+
+var kinds = map[string]rule.KindInfo{
+	"filegroup": {
+		NonEmptyAttrs:  map[string]bool{"srcs": true, "deps": true},
+		MergeableAttrs: map[string]bool{"srcs": true},
+	},
+}
+
+type swiftLang struct {
+	language.BaseLang
+
+	sawDone bool
+}
+
+func NewLanguage() language.Language {
+	return &swiftLang{}
+}
+
+func (*swiftLang) Name() string { return swiftName }
+
+func (*swiftLang) Kinds() map[string]rule.KindInfo {
+	return kinds
+}
+
+func (l *swiftLang) GenerateRules(args language.GenerateArgs) language.GenerateResult {
+	if l.sawDone {
+		panic("GenerateRules must not be called after DoneGeneratingRules")
+	}
+
+	r := rule.NewRule("filegroup", "all_files")
+	srcs := make([]string, 0, len(args.Subdirs)+len(args.RegularFiles))
+	srcs = append(srcs, args.RegularFiles...)
+	for _, f := range args.Subdirs {
+		pkg := path.Join(args.Rel, f)
+		srcs = append(srcs, "//"+pkg+":all_files")
+	}
+	r.SetAttr("srcs", srcs)
+	r.SetAttr("testonly", true)
+	if args.File == nil || !args.File.HasDefaultVisibility() {
+		r.SetAttr("visibility", []string{"//visibility:public"})
+	}
+	return language.GenerateResult{
+		Gen:     []*rule.Rule{r},
+		Imports: []interface{}{nil},
+	}
+}
+
+func (l *swiftLang) DoneGeneratingRules() {
+	l.sawDone = true
+}
+
+func (l *swiftLang) Resolve(c *config.Config, ix *resolve.RuleIndex, rc *repo.RemoteCache, r *rule.Rule, imports interface{}, from label.Label) {
+	if !l.sawDone {
+		panic("Expected a call to DoneGeneratingRules before Resolve")
+	}
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,11 @@
 module github.com/cgrindel/swift_bazel
 
 go 1.19
+
+require github.com/bazelbuild/bazel-gazelle v0.28.0
+
+require (
+	github.com/bazelbuild/buildtools v0.0.0-20221004120235-7186f635531b // indirect
+	golang.org/x/sys v0.1.0 // indirect
+	golang.org/x/tools v0.2.0 // indirect
+)
