52d679bbc10cb2c0aa0a0af3addc97ef14cee605
Chuck Grindel <chuck@chuckgrindel.com>
2022-11-12 08:37:13 -0700

    Add CountLines to have multiple imports

---
+++ gazelle/resolve.go
@@ -0,0 +1,26 @@
+package gazelle
+
+import (
+	"log"
+
+	"github.com/bazelbuild/bazel-gazelle/config"
+	"github.com/bazelbuild/bazel-gazelle/label"
+	"github.com/bazelbuild/bazel-gazelle/repo"
+	"github.com/bazelbuild/bazel-gazelle/resolve"
+	"github.com/bazelbuild/bazel-gazelle/rule"
+)
+
+func (l *swiftLang) Resolve(
+	c *config.Config,
+	ix *resolve.RuleIndex,
+	rc *repo.RemoteCache,
+	r *rule.Rule,
+	imports interface{},
+	from label.Label) {
+
+	// DEBUG BEGIN
+	log.Printf("*** CHUCK: Resolve r: %+#v", r)
+	log.Printf("*** CHUCK: Resolve imports: %+#v", imports)
+	log.Printf("*** CHUCK: Resolve from: %+#v", from)
+	// DEBUG END
+}
