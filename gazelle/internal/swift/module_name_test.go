62b0596b5849b690fe4e03e1f374cfb91fc57005
Chuck Grindel <chuck@chuckgrindel.com>
2022-11-15 08:52:28 -0700

    Add blank BUILD files to examples

---
+++ gazelle/internal/swift/module_name_test.go
@@ -0,0 +1,12 @@
+package swift_test
+
+import "testing"
+
+func TestModuleName(t *testing.T) {
+	t.Run("has module_name attribute", func(t *testing.T) {
+		t.Error("IMPLEMENT ME!")
+	})
+	t.Run("does not have module_name attribute", func(t *testing.T) {
+		t.Error("IMPLEMENT ME!")
+	})
+}
