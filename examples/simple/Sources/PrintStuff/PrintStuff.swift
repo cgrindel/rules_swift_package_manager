ec088f6ab520402fafe6f7b143b26696dcd2b056
Chuck Grindel <chuck@chuckgrindel.com>
2022-11-16 15:04:01 -0700

    Add missing Foundation

--- examples/simple/Sources/PrintStuff/PrintStuff.swift
+++ examples/simple/Sources/PrintStuff/PrintStuff.swift
@@ -1,11 +1,12 @@
 import ArgumentParser
+import Foundation
 import MyDequeModule
 
 @main
 struct PrintStuff: AsyncParsableCommand {
     mutating func run() async throws {
         fputs("My deque colors\n", stdout)
-        var colors = MyDeques.colors
+        let colors = MyDeques.colors
         for color in colors {
             fputs("color: \(String(reflecting: color))\n", stdout)
         }
