d8a18bb773565081301a9ccf8a19912fccd17d8b
Chuck Grindel <chuck@chuckgrindel.com>
2022-11-22 13:15:33 -0700

    Refactor config

--- gazelle/config.go
+++ gazelle/config.go
@@ -16,6 +16,8 @@ const swiftConfigName = "swift"
 type swiftConfig struct {
 	moduleIndex  *swift.ModuleIndex
 	swiftBinPath string
+	// If this is true and a Package.swift is found, then use it to generate Bazel build files.
+	genFromPkgManifest bool
 }
 
 func getSwiftConfig(c *config.Config) *swiftConfig {
@@ -27,6 +29,15 @@ func (*swiftLang) RegisterFlags(fs *flag.FlagSet, cmd string, c *config.Config) 
 	sc := &swiftConfig{
 		moduleIndex: swift.NewModuleIndex(),
 	}
+
+	fs.BoolVar(
+		&sc.genFromPkgManifest,
+		"gen_from_pkg_manifest",
+		false,
+		"If set and a Package.swift file is found, then generate build files from manifest info.",
+	)
+
+	// Store the config for later steps
 	c.Exts[swiftConfigName] = sc
 }
 
@@ -34,16 +45,40 @@ func (sl *swiftLang) CheckFlags(fs *flag.FlagSet, c *config.Config) error {
 	var err error
 	sc := getSwiftConfig(c)
 
-	// TODO(chuck): Add flag so that the client can tell use which Swift to use.
+	// TODO(chuck): Add flag so that the client can tell us which Swift to use.
 
 	// Find the Swift executable
-	sc.swiftBinPath, err = swiftbin.FindSwiftBinPath()
-	if err != nil {
+	if sc.swiftBinPath, err = swiftbin.FindSwiftBinPath(); err != nil {
 		return err
 	}
 
 	// Look for http_archive declarations with Swift declarations.
-	wkspFilePath := wspace.FindWORKSPACEFile(c.RepoRoot)
+	if err = findExternalDepsInWorkspace(sc.moduleIndex, c.RepoRoot); err != nil {
+		return err
+	}
+
+	// wkspFilePath := wspace.FindWORKSPACEFile(c.RepoRoot)
+	// wkspFile, err := rule.LoadWorkspaceFile(wkspFilePath, "")
+	// if err != nil {
+	// 	return fmt.Errorf("failed to load WORKSPACE file %v: %w", wkspFilePath, err)
+	// }
+	// archives, err := swift.NewHTTPArchivesFromWkspFile(wkspFile)
+	// if err != nil {
+	// 	return fmt.Errorf(
+	// 		"failed to retrieve archives from workspace file %v: %w",
+	// 		wkspFilePath,
+	// 		err,
+	// 	)
+	// }
+	// for _, archive := range archives {
+	// 	sc.moduleIndex.AddModules(archive.Modules...)
+	// }
+
+	return nil
+}
+
+func findExternalDepsInWorkspace(mi *swift.ModuleIndex, repoRoot string) error {
+	wkspFilePath := wspace.FindWORKSPACEFile(repoRoot)
 	wkspFile, err := rule.LoadWorkspaceFile(wkspFilePath, "")
 	if err != nil {
 		return fmt.Errorf("failed to load WORKSPACE file %v: %w", wkspFilePath, err)
@@ -57,8 +92,7 @@ func (sl *swiftLang) CheckFlags(fs *flag.FlagSet, c *config.Config) error {
 		)
 	}
 	for _, archive := range archives {
-		sc.moduleIndex.AddModules(archive.Modules...)
+		mi.AddModules(archive.Modules...)
 	}
-
 	return nil
 }
