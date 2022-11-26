0a278357605dd30e44066e43a4b59d0733804524
Chuck Grindel <chuck@chuckgrindel.com>
2022-11-26 15:50:33 -0700

    Add tests for GenerateRulesMode

--- gazelle/internal/swiftcfg/swift_config_test.go
+++ gazelle/internal/swiftcfg/swift_config_test.go
@@ -3,8 +3,10 @@ package swiftcfg_test
 import (
 	"testing"
 
+	"github.com/bazelbuild/bazel-gazelle/language"
 	"github.com/cgrindel/swift_bazel/gazelle/internal/swiftbin"
 	"github.com/cgrindel/swift_bazel/gazelle/internal/swiftcfg"
+	"github.com/cgrindel/swift_bazel/gazelle/internal/swiftpkg"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -24,14 +26,26 @@ func TestSwiftConfigSwiftBin(t *testing.T) {
 }
 
 func TestSwiftConfigGenerateRulesMode(t *testing.T) {
+	sc := swiftcfg.NewSwiftConfig()
+	args := language.GenerateArgs{
+		Dir: "/path/bazel/pkg",
+	}
+
 	t.Run("no package info", func(t *testing.T) {
-		t.Error("IMPLEMENT ME!")
+		sc.PackageInfo = nil
+		assert.Equal(t, swiftcfg.SrcFileGenRulesMode, sc.GenerateRulesMode(args))
 	})
 	t.Run("has package info, args Dir is not the package dir", func(t *testing.T) {
-		t.Error("IMPLEMENT ME!")
+		sc.PackageInfo = &swiftpkg.PackageInfo{
+			Dir: "/path/bazel/pkg/subdir",
+		}
+		assert.Equal(t, swiftcfg.SkipGenRulesMode, sc.GenerateRulesMode(args))
 	})
 	t.Run("has package info, args Dir is the package dir", func(t *testing.T) {
-		t.Error("IMPLEMENT ME!")
+		sc.PackageInfo = &swiftpkg.PackageInfo{
+			Dir: "/path/bazel/pkg",
+		}
+		assert.Equal(t, swiftcfg.SwiftPkgGenRulesMode, sc.GenerateRulesMode(args))
 	})
 }
 
