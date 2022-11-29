e86770f9213d7a02bdf08e1ecf4027fe40784ec7
Chuck Grindel <chuck@chuckgrindel.com>
2022-11-29 12:45:58 -0700

    Add update_repos.go

---
+++ gazelle/update_repos.go
@@ -0,0 +1,9 @@
+package gazelle
+
+import "github.com/bazelbuild/bazel-gazelle/language"
+
+func (*swiftLang) UpdateRepos(args language.UpdateReposArgs) language.UpdateReposResult {
+	result := language.UpdateReposResult{}
+
+	return result
+}
