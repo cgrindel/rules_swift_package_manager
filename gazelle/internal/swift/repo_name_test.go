3fe7115db7ac046d0cef0f4ad74e9366afba6c9e
Chuck Grindel <chuck@chuckgrindel.com>
2022-12-01 11:35:21 -0700

    Add tests for RepoNameFromPin

--- gazelle/internal/swift/repo_name_test.go
+++ gazelle/internal/swift/repo_name_test.go
@@ -3,6 +3,7 @@ package swift_test
 import (
 	"testing"
 
+	"github.com/cgrindel/swift_bazel/gazelle/internal/spreso"
 	"github.com/cgrindel/swift_bazel/gazelle/internal/swift"
 	"github.com/stretchr/testify/assert"
 )
@@ -19,4 +20,36 @@ func TestRepoNameFromURL(t *testing.T) {
 	actual, err = swift.RepoNameFromURL("")
 	assert.ErrorContains(t, err, "URL cannot be empty string")
 	assert.Equal(t, "", actual)
+}
+
+func TestRepoNameFromStr(t *testing.T) {
+	actual := swift.RepoNameFromStr("swift-argument-parser")
+	assert.Equal(t, "swift_argument_parser", actual)
+}
+
+func TestRepoNameFromPin(t *testing.T) {
+	t.Run("pin is remoteSourceControl", func(t *testing.T) {
+		p := &spreso.Pin{
+			PkgRef: &spreso.PackageReference{
+				Kind:     spreso.RemoteSourceControlPkgRefKind,
+				Location: "https://github.com/apple/swift-argument-parser",
+				Identity: "swift-argument-parser",
+			},
+		}
+		actual, err := swift.RepoNameFromPin(p)
+		assert.NoError(t, err)
+		assert.Equal(t, "apple_swift_argument_parser", actual)
+	})
+	t.Run("pin is not remoteSourceControl", func(t *testing.T) {
+		p := &spreso.Pin{
+			PkgRef: &spreso.PackageReference{
+				Kind:     spreso.LocalSourceControlPkgRefKind,
+				Location: "/path/to/repo",
+				Identity: "swift-argument-parser",
+			},
+		}
+		actual, err := swift.RepoNameFromPin(p)
+		assert.NoError(t, err)
+		assert.Equal(t, "swift_argument_parser", actual)
+	})
 }
