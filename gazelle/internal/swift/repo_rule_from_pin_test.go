95f59b81498c9cd9b7d257bd39ca719e88c5002c
Chuck Grindel <chuck@chuckgrindel.com>
2022-12-02 08:36:45 -0700

    Add test for RepoRuleFromPin

--- gazelle/internal/swift/repo_rule_from_pin_test.go
+++ gazelle/internal/swift/repo_rule_from_pin_test.go
@@ -1,7 +1,34 @@
 package swift_test
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/cgrindel/swift_bazel/gazelle/internal/spreso"
+	"github.com/cgrindel/swift_bazel/gazelle/internal/swift"
+	"github.com/stretchr/testify/assert"
+)
 
 func TestRepoRuleFromPin(t *testing.T) {
-	t.Error("IMPLEMENT ME!")
+	remote := "https://github.com/apple/swift-argument-parser"
+	version := "1.2.3"
+	revision := "12345"
+	p := &spreso.Pin{
+		PkgRef: &spreso.PackageReference{
+			Location: remote,
+		},
+		State: &spreso.VersionPinState{
+			Version:  version,
+			Revision: revision,
+		},
+	}
+	actual, err := swift.RepoRuleFromPin(p)
+	assert.NoError(t, err)
+	assert.Equal(t, swift.SwiftPkgRuleKind, actual.Kind())
+	expectedName, err := swift.RepoNameFromPin(p)
+	assert.NoError(t, err)
+	assert.Equal(t, expectedName, actual.Name())
+	assert.Equal(t, remote, actual.AttrString("remote"))
+	assert.Equal(t, revision, actual.AttrString("commit"))
+	assert.Len(t, actual.Comments(), 1)
+	assert.Contains(t, actual.Comments()[0], "# version: 1.2.3")
 }
