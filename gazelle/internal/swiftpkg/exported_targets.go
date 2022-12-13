fefbe2f640a91ed76d58adf21214ea8028c9efae
Chuck Grindel <chuck@chuckgrindel.com>
2022-12-13 08:31:56 -0700

    Add GHXXX comment

--- gazelle/internal/swiftpkg/exported_targets.go
+++ gazelle/internal/swiftpkg/exported_targets.go
@@ -6,6 +6,8 @@ import (
 	"github.com/cgrindel/swift_bazel/gazelle/internal/spdesc"
 )
 
+// GH010: Add test for ExportedTargets
+
 // Targets that are made available outside of the package via a Product reference.
 func (pi *PackageInfo) ExportedTargets() ([]*spdesc.Target, error) {
 	m := pi.DescManifest
@@ -13,12 +15,13 @@ func (pi *PackageInfo) ExportedTargets() ([]*spdesc.Target, error) {
 	exported := make([]*spdesc.Target, len(m.Products))
 	for idx, p := range m.Products {
 		if len(p.Targets) == 0 {
-			return nil, fmt.Errorf("product %s has not targets", p.Name)
+			return nil, fmt.Errorf("product %s has no targets", p.Name)
 		}
 		targetName := p.Targets[0]
 		t := m.Targets.FindByName(targetName)
 		if t == nil {
-			return nil, fmt.Errorf("target %s not found while finding product module", targetName)
+			return nil, fmt.Errorf(
+				"target %s not found while generating exported targets", targetName)
 		}
 		exported[idx] = t
 	}
