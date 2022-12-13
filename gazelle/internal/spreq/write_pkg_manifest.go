1948b7de4517b0c42d9965c2ea74701ab34777d8
Chuck Grindel <chuck@chuckgrindel.com>
2022-12-13 09:22:28 -0700

    Remove spreq

--- gazelle/internal/spreq/write_pkg_manifest.go
+++
@@ -1,6 +0,0 @@
-package spreq
-
-func WritePkgManifest(reqs *Requirements, pkg_path string) error {
-	// TODO(chuck): IMPLEMENT ME!
-	return nil
-}
--- gazelle/update_repos.go
+++ gazelle/update_repos.go
@@ -21,30 +21,17 @@ const pkgManifestBasename = "Package.swift"
 const swiftPkgBuildDirname = ".build"
 const swiftPkgCheckoutsDirname = "checkouts"
 
-// var yamlExtensions = []string{".yaml", ".yml"}
-
 func (*swiftLang) CanImport(path string) bool {
 	return isPkgManifest(path)
-	// return isResolvedPkg(path) || isPkgManifest(path) || isSwiftReqs(path)
 }
-
-// func isResolvedPkg(path string) bool {
-// 	return filepath.Base(path) == resolvedPkgBasename
-// }
 
 func isPkgManifest(path string) bool {
 	return filepath.Base(path) == pkgManifestBasename
 }
 
-// func isSwiftReqs(path string) bool {
-// 	return stringslices.Contains(yamlExtensions, filepath.Ext(path))
-// }
-
 func (*swiftLang) ImportRepos(args language.ImportReposArgs) language.ImportReposResult {
 	if isPkgManifest(args.Path) {
 		return importReposFromPackageManifest(args)
-		// } else if isSwiftReqs(args.Path) {
-		// 	return importReposFromSwiftReqs(args)
 	}
 	log.Fatal("No handler found for ImportRepos.")
 	return language.ImportReposResult{}
@@ -137,35 +124,3 @@ func importReposFromResolvedPackage(
 
 	return result
 }
-
-// func importReposFromSwiftReqs(args language.ImportReposArgs) language.ImportReposResult {
-// 	result := language.ImportReposResult{}
-// 	c := args.Config
-// 	sc := swiftcfg.GetSwiftConfig(c)
-
-// 	b, err := os.ReadFile(args.Path)
-// 	if err != nil {
-// 		result.Error = err
-// 		return result
-// 	}
-// 	reqs, err := spreq.NewRequirementsFromYAML(b)
-// 	if err != nil {
-// 		result.Error = err
-// 		return result
-// 	}
-// 	pkgDir := filepath.Dir(args.Path)
-// 	err = spreq.WritePkgManifest(reqs, pkgDir)
-// 	if err != nil {
-// 		result.Error = err
-// 		return result
-// 	}
-// 	sb := sc.SwiftBin()
-// 	err = sb.Resolve(pkgDir)
-// 	if err != nil {
-// 		result.Error = err
-// 		return result
-// 	}
-
-// 	resolvedPkgPath := filepath.Join(pkgDir, resolvedPkgBasename)
-// 	return importReposFromResolvedPackage(resolvedPkgPath)
-// }
