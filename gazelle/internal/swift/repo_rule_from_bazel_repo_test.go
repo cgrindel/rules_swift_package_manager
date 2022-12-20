d4788d96fcedd6aaa605f32a7de63da65d0447b7
Chuck Grindel <chuck@chuckgrindel.com>
2022-12-20 09:06:24 -0700

    Add local_swift_package test for RepoRuleFromBazelRepo

--- gazelle/internal/swift/repo_rule_from_bazel_repo_test.go
+++ gazelle/internal/swift/repo_rule_from_bazel_repo_test.go
@@ -2,18 +2,21 @@ package swift_test
 
 import (
 	"fmt"
+	"path/filepath"
 	"testing"
 
 	"github.com/bazelbuild/bazel-gazelle/rule"
 	"github.com/cgrindel/swift_bazel/gazelle/internal/spreso"
 	"github.com/cgrindel/swift_bazel/gazelle/internal/swift"
+	"github.com/cgrindel/swift_bazel/gazelle/internal/swiftpkg"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestRepoRuleFromBazelRepo(t *testing.T) {
+	pkgDir := "/path/to/package"
+	miBasename := "module_index.json"
+
 	t.Run("with pin (source control dep)", func(t *testing.T) {
-		pkgDir := "/path/to/package"
-		miBasename := "module_index.json"
 		repoName := "swiftpkg_swift_argument_parser"
 		remote := "https://github.com/apple/swift-argument-parser"
 		version := "1.2.3"
@@ -43,6 +46,24 @@ func TestRepoRuleFromBazelRepo(t *testing.T) {
 		assert.Equal(t, expected, actual)
 	})
 	t.Run("without pin (local Swift package)", func(t *testing.T) {
-		t.Error("IMPLEMENT ME!")
+		pkgDir := "/path/to/package"
+		relLocalPkgDir := "third_party/cool_local_package"
+		localPkgDir := filepath.Join(pkgDir, relLocalPkgDir)
+		repoName := "swiftpkg_cool_local_package"
+
+		br := &swift.BazelRepo{
+			Name: repoName,
+			PkgInfo: &swiftpkg.PackageInfo{
+				Name: "cool-local-package",
+				Path: localPkgDir,
+			},
+		}
+		actual, err := swift.RepoRuleFromBazelRepo(br, miBasename, pkgDir)
+		assert.NoError(t, err)
+
+		expected := rule.NewRule(swift.LocalSwiftPkgRuleKind, repoName)
+		expected.SetAttr("path", relLocalPkgDir)
+		expected.SetAttr("module_index", fmt.Sprintf("@//:%s", miBasename))
+		assert.Equal(t, expected, actual)
 	})
 }
