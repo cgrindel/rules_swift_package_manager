caac9e0c4a3f344b5abbea3f7c80474b351cdb77
Chuck Grindel <chuck@chuckgrindel.com>
2023-01-02 10:02:31 -0700

    Add gazelle package comment

---
+++ gazelle/doc.go
@@ -0,0 +1,7 @@
+// Package gazelle implements a [Bazel Gazelle] extension/plugin for the [Swift] programming
+// language. For information on how to use the plugin, please see the [Swift Bazel documentation].
+//
+// [Bazel Gazelle]: https://github.com/bazelbuild/bazel-gazelle
+// [Swift]: https://www.swift.org/
+// [Swift Bazel documentation]: https://github.com/cgrindel/swift_bazel
+package gazelle
