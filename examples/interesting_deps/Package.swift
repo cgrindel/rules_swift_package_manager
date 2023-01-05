bd7a0dc69d1428fe1209b0f316395697606999e0
Chuck Grindel <chuck.grindel@gmail.com>
2023-01-05 10:53:51 -0700

    Remove local package ref

--- examples/interesting_deps/Package.swift
+++ examples/interesting_deps/Package.swift
@@ -6,7 +6,6 @@ let package = Package(
     name: "MySwiftPackage",
     dependencies: [
         .package(url: "https://github.com/apple/swift-log", from: "1.4.4"),
-        // .package(url: "https://github.com/SDWebImage/libwebp-Xcode.git", from: "1.2.1"),
-        .package(path: "/Users/chuck/code/cgrindel/libwebp-Xcode/cg/add_5_5_manifest"),
+        .package(url: "https://github.com/SDWebImage/libwebp-Xcode.git", from: "1.2.1"),
     ]
 )
