9591dc411e8266702c665b6d15537f6e92c3aa44
Chuck Grindel <chuck.grindel@gmail.com>
2023-01-06 09:05:33 -0700

    Initial files

---
+++ examples/xcmetrics/Package.swift
@@ -0,0 +1,10 @@
+// swift-tools-version: 5.7
+
+import PackageDescription
+
+let package = Package(
+    name: "",
+    dependencies: [
+        .package(url: "https://github.com/spotify/XCMetrics", from: "0.0.10"),
+    ]
+)
