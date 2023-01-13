d66ae72bd7871b7135b4c497792d102daa92f8e7
Chuck Grindel <chuck.grindel@gmail.com>
2023-01-13 09:10:57 -0700

    Doc for spdesc/manifest.go

--- gazelle/internal/spdesc/manifest.go
+++ gazelle/internal/spdesc/manifest.go
@@ -1,3 +1,4 @@
+// Package spdesc provides types and utility functions for reading Swift package description JSON.
 package spdesc
 
 import (
@@ -7,6 +8,7 @@ import (
 
 // The JSON formats described in this file are for the `swift package describe --type json` JSON output.
 
+// NewManifestFromJSON creates a Swift manifest from description JSON.
 func NewManifestFromJSON(bytes []byte) (*Manifest, error) {
 	var manifest Manifest
 	err := json.Unmarshal(bytes, &manifest)
@@ -18,6 +20,8 @@ func NewManifestFromJSON(bytes []byte) (*Manifest, error) {
 
 // Manifest
 
+// A Manifest represents the root of the description JSON. The members of the struct provide access
+// to the Swift manifest parts.
 type Manifest struct {
 	Name                string
 	ManifestDisplayName string `json:"manifest_display_name"`
@@ -31,8 +35,11 @@ type Manifest struct {
 
 // Targets
 
+// Targets represents a slice of Swift targets.
 type Targets []Target
 
+// FindByName returns the Swift target that matches the provided name. It returns nil, if a match is
+// not found.
 func (ts Targets) FindByName(name string) *Target {
 	for _, t := range ts {
 		if t.Name == name {
@@ -42,6 +49,8 @@ func (ts Targets) FindByName(name string) *Target {
 	return nil
 }
 
+// FindByPath returns the Swift target that matches the provided path. It returns nil, if a match is
+// not found.
 func (ts Targets) FindByPath(path string) *Target {
 	for _, t := range ts {
 		if t.Path == path {
@@ -51,6 +60,7 @@ func (ts Targets) FindByPath(path string) *Target {
 	return nil
 }
 
+// A Target represents a Swift target.
 type Target struct {
 	Name                string
 	C99name             string `json:"c99name"`
@@ -63,6 +73,7 @@ type Target struct {
 	ProductMemberships  []string `json:"product_memberships"`
 }
 
+// SourcesWithPath returns the sources prepended by the target's path.
 func (t *Target) SourcesWithPath() []string {
 	if t.Path == "" {
 		return t.Sources
@@ -76,6 +87,7 @@ func (t *Target) SourcesWithPath() []string {
 
 // Platforms
 
+// A Platform represents a Swift package platform.
 type Platform struct {
 	Name    string
 	Version string
@@ -83,6 +95,7 @@ type Platform struct {
 
 // Product
 
+// A ProductType is an enum that identifies the type of Swift product.
 type ProductType int
 
 const (
@@ -108,6 +121,7 @@ func (pt *ProductType) UnmarshalJSON(b []byte) error {
 	return nil
 }
 
+// A Product represents a Swift product.
 type Product struct {
 	Name    string
 	Targets []string
@@ -116,6 +130,7 @@ type Product struct {
 
 // Dependency
 
+// A Dependency represents a Swift external dependency.
 type Dependency struct {
 	Identity    string
 	Type        string
@@ -125,10 +140,12 @@ type Dependency struct {
 
 // Requirement
 
+// A DependencyRequirement represents the eligibility requirements for an external dependency.
 type DependencyRequirement struct {
 	Range []VersionRange
 }
 
+// A VersionRange represents an upper and lower bound for the elgibility of an external dependency.
 type VersionRange struct {
 	LowerBound string `json:"lower_bound"`
 	UpperBound string `json:"upper_bound"`
