16fbae6243943c66087cb55412b3ed61ff01ffa5
Chuck Grindel <chuck.grindel@gmail.com>
2023-02-16 09:49:25 -0700

    Added Foo that uses SimpleCore. Recreates the @import issue.

--- swiftpkg/tests/generate_modulemap_tests/Sources/PrintVersion/PrintVersion.swift
+++ swiftpkg/tests/generate_modulemap_tests/Sources/PrintVersion/PrintVersion.swift
@@ -1,9 +1,16 @@
+import Foo
 import SimpleCore
 
 @main
 enum PrintVersion {
     static func main() {
         let verInfo = VersionInfo()
-        print(verInfo.version)
+        let fooVerInfo = FooVersionInfo()
+        if verInfo.version == fooVerInfo.version {
+            print(verInfo.version)
+        } else {
+            print("Versions do not match")
+            print("verInfo: \(verInfo.version), fooVerInfo: \(fooVerInfo.version)")
+        }
     }
 }
