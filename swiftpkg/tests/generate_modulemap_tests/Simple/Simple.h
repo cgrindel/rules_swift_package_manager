e5a63a861ec8490342b353ca59a68c88f3e24456
Chuck Grindel <chuck.grindel@gmail.com>
2023-02-16 16:43:41 -0700

    Remove Simple.

--- swiftpkg/tests/generate_modulemap_tests/Simple/Simple.h
+++
@@ -1,3 +0,0 @@
-// Puprosefully importing a header that is not part of the Simple target.
-// Mimics Firebase target in firebase-ios-sdk.
-#import <SimpleCore/SimpleCore.h>
--- swiftpkg/tests/generate_modulemap_tests/Simple/dummy.m
+++
@@ -1,2 +0,0 @@
-// Intentionally blank
-
