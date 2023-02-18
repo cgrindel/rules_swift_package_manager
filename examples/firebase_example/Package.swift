2a9ea6bb54be992e4df36f5fb3730241451b7148
Chuck Grindel <chuck.grindel@gmail.com>
2023-02-18 12:42:32 -0800

    Upgrade firebase to 10.x

--- examples/firebase_example/Package.swift
+++ examples/firebase_example/Package.swift
@@ -5,6 +5,7 @@ import PackageDescription
 let package = Package(
     name: "firebase_example",
     dependencies: [
-        .package(url: "https://github.com/firebase/firebase-ios-sdk", .exact("8.9.1")),
+        // .package(url: "https://github.com/firebase/firebase-ios-sdk", .exact("8.9.1")),
+        .package(url: "https://github.com/firebase/firebase-ios-sdk", .upToNextMajor(from: "10.0.0")),
     ]
 )
