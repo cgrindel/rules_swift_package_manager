1b671bc6545e4c8c255bc981b190d0e11367704e
Chuck Grindel <chuck.grindel@gmail.com>
2023-02-24 11:48:01 -0700

    Added phone_number_kit example

---
+++ examples/phone_number_kit/README.md
@@ -0,0 +1,3 @@
+# PhoneNumberKit Example
+
+This example demonstrates support for resources defined in Swift package manifest.
---
+++ examples/phone_number_kit/Tests/PhoneNumberKitTests/ParseTests.swift
@@ -0,0 +1,8 @@
+@testable import PhoneNumberKit
+import XCTest
+
+class ParseTests: XCTestCase {
+    func test_parse() throws {
+        fail("IMPLEMENT ME!")
+    }
+}
