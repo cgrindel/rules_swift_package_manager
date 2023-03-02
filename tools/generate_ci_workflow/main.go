8a6640bf6b99bcdcc38f56761bd28e1c2ae8fb54
Chuck Grindel <chuck.grindel@gmail.com>
2023-03-02 10:05:14 -0700

    Initial files for generator

---
+++ tools/generate_ci_workflow/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"os/signal"
+)
+
+func main() {
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer cancel()
+	if err := run(ctx, os.Stderr); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+}
+
+func run(ctx context.Context, stderr *os.File) error {
+	fmt.Printf("MADE IT")
+	return nil
+}
