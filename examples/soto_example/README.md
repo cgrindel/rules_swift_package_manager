190917102e272a828c5e780d2bc9cbb62b011bf7
Chuck Grindel <chuck.grindel@gmail.com>
2023-03-03 08:35:50 -0700

    Initial soto example files

---
+++ examples/soto_example/README.md
@@ -0,0 +1,3 @@
+# PhoneNumberKit Example
+
+This example demonstrates support for resources defined in a Swift package manifest.
---
+++ examples/soto_example/Tests/SotoTests/SotoTests.swift
@@ -0,0 +1,10 @@
+@testable import SotoS3
+import XCTest
+
+class SotoTests: XCTestCase {
+    func testSomething() {
+        let bucketName = "soto-getting-started-bucket"
+        let createBucketRequest = S3.CreateBucketRequest(bucket: bucketName)
+        XCTAssertEqual(createBucketRequest.bucket, bucketName)
+    }
+}
