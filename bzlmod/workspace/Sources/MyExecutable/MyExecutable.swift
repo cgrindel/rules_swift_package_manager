9792673f74cb33ce0b4742740b3566e34f03fb72
Chuck Grindel <chuck.grindel@gmail.com>
2023-03-28 09:58:05 -0600

    bzlmod e2e test works

--- bzlmod/workspace/Sources/MyExecutable/MyExecutable.swift
+++ bzlmod/workspace/Sources/MyExecutable/MyExecutable.swift
@@ -4,7 +4,7 @@ import MyLibrary
 @main
 struct MyExecutable: AsyncParsableCommand {
     mutating func run() async throws {
-        let output = "Hello, \(World())"
+        let output = "Hello, \(World().name)!"
         print(output)
     }
 }
