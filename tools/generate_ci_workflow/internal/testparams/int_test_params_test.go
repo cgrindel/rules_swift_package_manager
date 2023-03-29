015fb56ab5948c49271bdf8abd77efa152645968
Chuck Grindel <chuck.grindel@gmail.com>
2023-03-29 12:31:45 -0600

    Remove example package.

--- tools/generate_ci_workflow/internal/testparams/int_test_params_test.go
+++ tools/generate_ci_workflow/internal/testparams/int_test_params_test.go
@@ -3,19 +3,24 @@ package testparams_test
 import (
 	"testing"
 
-	"github.com/cgrindel/swift_bazel/tools/generate_ci_workflow/internal/example"
+	"github.com/cgrindel/swift_bazel/tools/generate_ci_workflow/internal/testparams"
 	"github.com/stretchr/testify/assert"
 )
 
-func TestNewExamplesFromJSON(t *testing.T) {
-	examples, err := example.NewExamplesFromJSON([]byte(intTestParamsJSON))
+func TestNewIntTestParamsFromJSON(t *testing.T) {
+	actual, err := testparams.NewIntTestParamsFromJSON([]byte(intTestParamsJSON))
 	assert.NoError(t, err)
-	assert.Len(t, examples, 2)
+	assert.Len(t, actual, 2)
+	expected := []testparams.IntTestParams{
+		{Test: "@@//path:int_test", OS: "macos", BzlmodMode: "enabled"},
+		{Test: "@@//path:int_test", OS: "linux", BzlmodMode: "disabled"},
+	}
+	assert.Equal(t, expected, actual)
 }
 
 const intTestParamsJSON = `
 [
-  {"test": "@@//path:int_test", "os": "macos", "enable_bzlmod": true},
-  {"test": "@@//path:int_test", "os": "linux", "enable_bzlmod": true}
+  {"test": "@@//path:int_test", "os": "macos", "bzlmod_mode": "enabled"},
+  {"test": "@@//path:int_test", "os": "linux", "bzlmod_mode": "disabled"}
 ]
 `
