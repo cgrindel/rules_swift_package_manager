bb5a744cddb31d9cb97299cba59c0ed481997b4d
Chuck Grindel <chuck.grindel@gmail.com>
2023-03-30 09:39:12 -0600

    Removed unused files. Saw app work in simulator

--- examples/resources_example/third_party/package_with_resources/Sources/CoolUI/Resources.swift
+++
@@ -1,9 +0,0 @@
-import UIKit
-
-func moduleLocalized(_ key: String) -> String {
-    NSLocalizedString(key, bundle: .module, comment: "")
-}
-
-func moduleImage(_ name: String) -> UIImage? {
-    UIImage(named: name, in: .module, with: nil)
-}
