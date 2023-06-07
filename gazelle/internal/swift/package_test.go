d79e1b3b56a8e6978e5edc26322613d3429537d3
Chuck Grindel <chuck.grindel@gmail.com>
2023-06-07 16:20:34 -0600

    Add tests to ensure patch information is applied correcctly to swift_package.

--- gazelle/internal/swift/package_test.go
+++ gazelle/internal/swift/package_test.go
@@ -48,6 +48,45 @@ func TestNewPackageFromBazelRepo(t *testing.T) {
 		}
 		assert.Equal(t, expected, actual)
 	})
+	t.Run("with pin (source control dep) and patch", func(t *testing.T) {
+		repoName := "swiftpkg_swift_argument_parser"
+		identity := "swift-argument-parser"
+		remote := "https://github.com/apple/swift-argument-parser"
+		version := "1.2.3"
+		revision := "12345"
+		p := &spreso.Pin{
+			PkgRef: &spreso.PackageReference{
+				Kind:     spreso.RemoteSourceControlPkgRefKind,
+				Location: remote,
+			},
+			State: &spreso.VersionPinState{
+				Version:  version,
+				Revision: revision,
+			},
+		}
+		br := &swift.BazelRepo{
+			Name:     repoName,
+			Identity: identity,
+			Pin:      p,
+		}
+		patch := &swift.Patch{
+			Args:  []string{"-p1"},
+			Files: []string{"@@//third-party/foo:0001-fix.patch"},
+		}
+		actual, err := swift.NewPackageFromBazelRepo(br, diBasename, pkgDir, patch)
+		assert.NoError(t, err)
+		expected := &swift.Package{
+			Name:     repoName,
+			Identity: identity,
+			Remote: &swift.RemotePackage{
+				Commit:  revision,
+				Remote:  remote,
+				Version: version,
+				Patch:   patch,
+			},
+		}
+		assert.Equal(t, expected, actual)
+	})
 	t.Run("without pin (local Swift package)", func(t *testing.T) {
 		pkgDir := "/path/to/package"
 		relLocalPkgDir := "third_party/cool_local_package"
--- gazelle/internal/swift/repo_rule_from_bazel_repo_test.go
+++ gazelle/internal/swift/repo_rule_from_bazel_repo_test.go
@@ -17,8 +17,6 @@ func TestRepoRuleFromBazelRepo(t *testing.T) {
 	pkgDir := "/path/to/repo"
 	diBasename := "swift_deps_index.json"
 
-	// TODO(chuck): Add test with patch
-
 	t.Run("with pin (source control dep)", func(t *testing.T) {
 		repoName := "swiftpkg_swift_argument_parser"
 		remote := "https://github.com/apple/swift-argument-parser"
@@ -46,6 +44,41 @@ func TestRepoRuleFromBazelRepo(t *testing.T) {
 		expected.SetAttr("remote", remote)
 		expected.SetAttr("dependencies_index", fmt.Sprintf("@//:%s", diBasename))
 		expected.AddComment("# version: 1.2.3")
+		assert.Equal(t, expected, actual)
+	})
+	t.Run("with pin (source control dep) and patch", func(t *testing.T) {
+		repoName := "swiftpkg_swift_argument_parser"
+		remote := "https://github.com/apple/swift-argument-parser"
+		version := "1.2.3"
+		revision := "12345"
+		p := &spreso.Pin{
+			PkgRef: &spreso.PackageReference{
+				Kind:     spreso.RemoteSourceControlPkgRefKind,
+				Location: remote,
+			},
+			State: &spreso.VersionPinState{
+				Version:  version,
+				Revision: revision,
+			},
+		}
+		br := &swift.BazelRepo{
+			Name: repoName,
+			Pin:  p,
+		}
+		patch := &swift.Patch{
+			Args:  []string{"-p1"},
+			Files: []string{"@@//third-party/foo:0001-fix.patch"},
+		}
+		actual, err := swift.RepoRuleFromBazelRepo(br, diBasename, pkgDir, repoDir, patch)
+		assert.NoError(t, err)
+
+		expected := rule.NewRule(swift.SwiftPkgRuleKind, repoName)
+		expected.SetAttr("commit", revision)
+		expected.SetAttr("remote", remote)
+		expected.SetAttr("dependencies_index", fmt.Sprintf("@//:%s", diBasename))
+		expected.AddComment("# version: 1.2.3")
+		expected.SetAttr("patch_args", []string{"-p1"})
+		expected.SetAttr("patches", []string{"@@//third-party/foo:0001-fix.patch"})
 		assert.Equal(t, expected, actual)
 	})
 	t.Run("without pin (local Swift package)", func(t *testing.T) {
