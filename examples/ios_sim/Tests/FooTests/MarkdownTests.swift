b24a3107015622a20fe0af4d6ba650d705b381c2
Chuck Grindel <chuck.grindel@gmail.com>
2023-06-07 12:12:04 -0600

    Remove purposeful failing test

--- examples/ios_sim/Tests/FooTests/MarkdownTests.swift
+++ examples/ios_sim/Tests/FooTests/MarkdownTests.swift
@@ -5,7 +5,6 @@ class MarkdownTests: XCTestCase {
     func test_parsing() throws {
         let source = "This is a markup *document*."
         let document = Document(parsing: source)
-        // XCTAssertNotEqual(document.debugDescription(), "")
-        XCTAssertEqual(document.debugDescription(), "")
+        XCTAssertNotEqual(document.debugDescription(), "")
     }
 }
