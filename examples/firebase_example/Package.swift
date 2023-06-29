3875dec27ef28216368b1ab635f3f55b5a838386
Chuck Grindel <chuck.grindel@gmail.com>
2023-06-29 13:50:49 -0600

    Add Reachability dep

--- examples/firebase_example/Package.swift
+++ examples/firebase_example/Package.swift
@@ -6,5 +6,7 @@ let package = Package(
     name: "firebase_example",
     dependencies: [
         .package(url: "https://github.com/firebase/firebase-ios-sdk", .upToNextMajor(from: "10.0.0")),
+        // Used by crashlytics example
+        .package(url: "https://github.com/ashleymills/Reachability.swift.git", .upToNextMajor(from: "5.1.0")),
     ]
 )
