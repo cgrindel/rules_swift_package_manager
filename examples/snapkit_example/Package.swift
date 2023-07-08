c6682d718cb1bccb68a94d357e97de1ea929e5df
Chuck Grindel <chuck.grindel@gmail.com>
2023-07-08 15:27:41 -0600

    chore: add `Snapkit` example (#460)
    - Add `snapkit_example`.

---
+++ examples/snapkit_example/Package.swift
@@ -0,0 +1,10 @@
+// swift-tools-version: 5.7
+
+import PackageDescription
+
+let package = Package(
+    name: "snapkit_example",
+    dependencies: [
+        .package(url: "https://github.com/SnapKit/SnapKit.git", .upToNextMajor(from: "5.0.1")),
+    ]
+)
