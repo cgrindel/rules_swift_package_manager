31f9855c51777567f27fde25d66eaf90ae236255
Chuck Grindel <chuck.grindel@gmail.com>
2023-07-08 13:39:46 -0600

    Initial snapkit_example files.

---
+++ examples/snapkit_example/Package.swift
@@ -0,0 +1,12 @@
+// swift-tools-version: 5.7
+
+import PackageDescription
+
+let package = Package(
+    name: "firebase_example",
+    dependencies: [
+        .package(url: "https://github.com/firebase/firebase-ios-sdk", .upToNextMajor(from: "10.11.0")),
+        // Used by crashlytics example
+        .package(url: "https://github.com/ashleymills/Reachability.swift.git", .upToNextMajor(from: "5.1.0")),
+    ]
+)
