854bc1d9553edb07f1d34add2c1ef1c600114db9
Chuck Grindel <chuck.grindel@gmail.com>
2023-07-13 06:03:14 -0600

    Remove unused Swift code

--- examples/ios_sim/Sources/Foo/MarkdownFoo.swift
+++ examples/ios_sim/Sources/Foo/MarkdownFoo.swift
@@ -5,7 +5,6 @@ class MarkdownExample {
     func test_parsing() throws {
         let source = "This is a markup *document*."
         let document = Document(parsing: source)
-        document.isEmpty
         print(document)
     }
 }
