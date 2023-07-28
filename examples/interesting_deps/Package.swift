0a3ac374be58adef061eba8decc330667612ce11
Edmund Kohlwey <ekohlwey@gmail.com>
2023-07-28 15:33:10 -0400

    Reproduce #504 Package with Two Products, with an Intradependency from One Product

--- examples/interesting_deps/Package.swift
+++ examples/interesting_deps/Package.swift
@@ -7,5 +7,6 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/apple/swift-log", from: "1.5.2"),
         .package(url: "https://github.com/SDWebImage/libwebp-Xcode.git", from: "1.3.1"),
+        .package(url: "https://github.com/scaraux/Swift-Porter-Stemmer-2.git", from: "0.1.1"),
     ]
 )
--- examples/interesting_deps/main.swift
+++ examples/interesting_deps/main.swift
@@ -1,8 +1,12 @@
 import libwebp
 import Logging
+import PorterStemmer2
 
 let logger = Logger(label: "com.example.main")
 logger.info("Hello World!")
 
 let webpVersion = WebPGetDecoderVersion()
 logger.info("WebP version: \(webpVersion)")
+
+let stemmer = PorterStemmer(withLanguage: .English)
+logger.info("Stemmer: \(stemmer.stem("running"))")
