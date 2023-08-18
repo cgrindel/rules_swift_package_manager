fedb6d7ca5d28b8c76d79a48de769940b930ecf7
Chuck Grindel <chuck.grindel@gmail.com>
2023-08-18 13:43:27 -0600

    Bazel build works.

--- examples/grpc_example/Package.swift
+++ examples/grpc_example/Package.swift
@@ -6,5 +6,6 @@ let package = Package(
     name: "grpc_example",
     dependencies: [
         // .package(url: "https://github.com/grpc/grpc-swift", from: "1.16.0"),
+        // .package(url: "https://github.com/apple/swift-log", from: "1.5.3"),
     ]
 )
