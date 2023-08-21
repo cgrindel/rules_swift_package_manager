be9972d413cc1e7f5b066577d14a2b7d0aa4dfbd
Chuck Grindel <chuck.grindel@gmail.com>
2023-08-21 11:24:38 -0600

    Remove commented code. Add comment about where the GRPC deps are sourced.

--- examples/grpc_example/Package.swift
+++ examples/grpc_example/Package.swift
@@ -5,6 +5,7 @@ import PackageDescription
 let package = Package(
     name: "grpc_example",
     dependencies: [
-        // .package(url: "https://github.com/grpc/grpc-swift", from: "1.16.0"),
+        // NOTE: The https://github.com/grpc/grpc-swift are currently coming from rules_swift.
+        // Related Slack thread: https://bazelbuild.slack.com/archives/CD3QY5C2X/p1692055426375909
     ]
 )
