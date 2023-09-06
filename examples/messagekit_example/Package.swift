de3ed948781612695b3448d4454fad63a97e78f2
Chuck Grindel <chuck.grindel@gmail.com>
2023-09-06 17:22:13 -0600

    Upgrade to latest messagekit

--- examples/messagekit_example/Package.swift
+++ examples/messagekit_example/Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let package = Package(
     name: "messagekit_example",
     dependencies: [
-        .package(url: "https://github.com/MessageKit/MessageKit", from: "3.7.0"),
+        .package(url: "https://github.com/MessageKit/MessageKit", from: "4.2.0"),
         .package(url: "https://github.com/onevcat/Kingfisher", from: "5.15.8"),
     ]
 )
