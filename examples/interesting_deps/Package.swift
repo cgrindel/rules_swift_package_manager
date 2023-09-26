6ec0c837e82aca1054b7224e0c8e53649b3389ed
Chuck Grindel <chuck.grindel@gmail.com>
2023-09-26 17:43:20 -0600

    Add CocoaLumberjack to the interesting_deps example.

--- examples/interesting_deps/Package.swift
+++ examples/interesting_deps/Package.swift
@@ -7,5 +7,6 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/apple/swift-log", from: "1.5.3"),
         .package(url: "https://github.com/SDWebImage/libwebp-Xcode.git", from: "1.3.2"),
+        .package(url: "https://github.com/CocoaLumberjack/CocoaLumberjack.git", from: "3.8.0"),
     ]
 )
