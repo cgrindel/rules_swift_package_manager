48f89cc018f006172c0216e7d8dbf74ec3a52d3a
Chuck Grindel <chuck.grindel@gmail.com>
2023-09-27 17:52:20 -0600

    Remove swift-log from shake_ios example

--- examples/shake_ios_example/Package.swift
+++ examples/shake_ios_example/Package.swift
@@ -5,7 +5,6 @@ import PackageDescription
 let package = Package(
     name: "shake_ios_example",
     dependencies: [
-        .package(url: "https://github.com/apple/swift-log", from: "1.5.3"),
         .package(url: "https://github.com/shakebugs/shake-ios", from: "16.2.2"),
     ]
 )
