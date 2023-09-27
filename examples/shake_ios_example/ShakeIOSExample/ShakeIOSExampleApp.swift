22b9f3c12e5086ac51763ba299a38d74da2e434e
Chuck Grindel <chuck.grindel@gmail.com>
2023-09-27 14:13:04 -0600

    Generated a SwiftUI app

---
+++ examples/shake_ios_example/ShakeIOSExample/ShakeIOSExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShakeIOSExampleApp.swift
+//  ShakeIOSExample
+//
+//  Created by Chuck Grindel on 9/27/23.
+//
+
+import SwiftUI
+
+@main
+struct ShakeIOSExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
