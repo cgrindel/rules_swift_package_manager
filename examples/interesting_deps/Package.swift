21ebb098e2af1044ef790f6b88bc663e9ecb9c84
Chuck Grindel <chuck.grindel@gmail.com>
2023-10-24 10:08:01 -0600

    Add GEOSwift to interesting_deps

--- examples/interesting_deps/Package.swift
+++ examples/interesting_deps/Package.swift
@@ -5,9 +5,10 @@ import PackageDescription
 let package = Package(
     name: "MySwiftPackage",
     dependencies: [
-        .package(url: "https://github.com/apple/swift-log", from: "1.5.3"),
-        .package(url: "https://github.com/SDWebImage/libwebp-Xcode.git", from: "1.3.2"),
         .package(url: "https://github.com/CocoaLumberjack/CocoaLumberjack.git", from: "3.8.1"),
+        .package(url: "https://github.com/GEOSwift/GEOSwift", from: "10.1.0"),
         .package(url: "https://github.com/OpenCombine/OpenCombine", from: "0.14.0"),
+        .package(url: "https://github.com/SDWebImage/libwebp-Xcode.git", from: "1.3.2"),
+        .package(url: "https://github.com/apple/swift-log", from: "1.5.3"),
     ]
 )
