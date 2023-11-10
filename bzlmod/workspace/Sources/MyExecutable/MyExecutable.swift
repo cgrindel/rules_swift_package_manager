ec6421e0d2ceb038d3b2df6fdc1329d24fd431eb
Luis Padron <lpadron@squareup.com>
2023-11-10 11:39:53 -0500

    chore: extend argument parser example
    Signed-off-by: Luis Padron <lpadron@squareup.com>

--- bzlmod/workspace/Sources/MyExecutable/MyExecutable.swift
+++ bzlmod/workspace/Sources/MyExecutable/MyExecutable.swift
@@ -1,10 +1,14 @@
 import ArgumentParser
+import Logging
 import MyLibrary
+import RegexBuilder
 
 @main
 struct MyExecutable: AsyncParsableCommand {
+    struct Plugin { }
     mutating func run() async throws {
         let output = "Hello, \(World().name)!"
-        print(output)
+        let logger = Logger(label: "com.example.BestExampleApp.main")
+        logger.info("\(output)")
     }
 }
