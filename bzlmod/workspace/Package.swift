47e4e462ebdb8dd59bd529b9d120c8050a81e251
Luis Padron <heyluispadron@gmail.com>
2023-11-10 16:11:08 -0500

    chore: extend argument parser example (#728)
    Extends the `MyExecutable` example to demonstrate a bug when `import
    RegexBuilder` is used. It seems to be unable to find the dependency
    label for `RegexBuilder` and then erroneously adds a dep for
    `"@swiftpkg_swift_argument_parser//:Plugins_GenerateManual"` which fails
    to build.
    
    Signed-off-by: Luis Padron <lpadron@squareup.com>

--- bzlmod/workspace/Package.swift
+++ bzlmod/workspace/Package.swift
@@ -4,8 +4,35 @@ import PackageDescription
 
 let package = Package(
     name: "MySwiftPackage",
+    products: [
+        .executable(name: "my-executable", targets: ["MyExecutable"]),
+    ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-argument-parser", from: "1.2.3"),
         .package(url: "https://github.com/apple/swift-log", from: "1.5.3"),
+    ],
+    targets: [
+        .executableTarget(
+            name: "MyExecutable",
+            dependencies: [
+                .product(name: "ArgumentParser", package: "swift-argument-parser"),
+                .product(name: "Logging", package: "swift-log"),
+                "MyLibrary",
+            ],
+            path: "Sources/MyExecutable",
+            exclude: ["BUILD.bazel"]
+        ),
+        .target(
+            name: "MyLibrary",
+            dependencies: [],
+            path: "Sources/MyLibrary",
+            exclude: ["BUILD.bazel"]
+        ),
+        .testTarget(
+            name: "MyLibraryTests",
+            dependencies: ["MyLibrary"],
+            path: "Tests/MyLibraryTests",
+            exclude: ["BUILD.bazel"]
+        )
     ]
 )
