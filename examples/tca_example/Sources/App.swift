4f4f58fad5d6ff6ef121cdface8c6dba9f96c4ab
JP Simard <jp@jpsim.com>
2023-11-13 18:29:53 -0500

    Update TCA to 1.4.0

--- examples/tca_example/Sources/App.swift
+++ examples/tca_example/Sources/App.swift
@@ -4,14 +4,13 @@ import Foundation
 import ComposableArchitecture
 import SwiftUI
 
-struct Feature: Reducer {
+@Reducer
+struct Feature {
     struct State: Equatable {
         var count = 0
         var numberFactAlert: String?
     }
-
-    @CasePathable
-    enum Action: Equatable {
+    enum Action {
         case factAlertDismissed
         case decrementButtonTapped
         case incrementButtonTapped
@@ -19,33 +18,35 @@ struct Feature: Reducer {
         case numberFactResponse(String)
     }
 
-    func reduce(into state: inout State, action: Action) -> Effect<Action> {
-        switch action {
-        case .factAlertDismissed:
-            state.numberFactAlert = nil
-            return .none
+    var body: some Reducer<State, Action> {
+        Reduce { state, action in
+            switch action {
+            case .factAlertDismissed:
+                state.numberFactAlert = nil
+                return .none
 
-        case .decrementButtonTapped:
-            state.count -= 1
-            return .none
+            case .decrementButtonTapped:
+                state.count -= 1
+                return .none
 
-        case .incrementButtonTapped:
-            state.count += 1
-            return .none
+            case .incrementButtonTapped:
+                state.count += 1
+                return .none
 
-        case .numberFactButtonTapped:
-            return .run { [count = state.count] send in
-                let (data, _) = try await URLSession.shared.data(
-                    from: URL(string: "http://numbersapi.com/\(count)/trivia")!
-                )
-                await send(
-                    .numberFactResponse(String(decoding: data, as: UTF8.self))
-                )
+            case .numberFactButtonTapped:
+                return .run { [count = state.count] send in
+                    let (data, _) = try await URLSession.shared.data(
+                        from: URL(string: "http://numbersapi.com/\(count)/trivia")!
+                    )
+                    await send(
+                        .numberFactResponse(String(decoding: data, as: UTF8.self))
+                    )
+                }
+
+            case let .numberFactResponse(fact):
+                state.numberFactAlert = fact
+                return .none
             }
-
-        case let .numberFactResponse(fact):
-            state.numberFactAlert = fact
-            return .none
         }
     }
 }
