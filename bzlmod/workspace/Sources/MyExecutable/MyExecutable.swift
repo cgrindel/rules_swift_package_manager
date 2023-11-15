042a92fed6c6477e98b5a9515a808a9544bdee50
Luis Padron <lpadron@squareup.com>
2023-11-15 00:10:46 -0500

    fix: only add dependency when unresolved

--- bzlmod/workspace/Sources/MyExecutable/MyExecutable.swift
+++ bzlmod/workspace/Sources/MyExecutable/MyExecutable.swift
@@ -3,6 +3,11 @@ import Logging
 import MyLibrary
 import RegexBuilder
 
+// Because we build on macos/linux this needs to be conditionally imported to test an edge case
+#if canImport(os)
+import os
+#endif
+
 @main
 struct MyExecutable: AsyncParsableCommand {
     struct Plugin { }
@@ -10,5 +15,9 @@ struct MyExecutable: AsyncParsableCommand {
         let output = "Hello, \(World().name)!"
         let logger = Logger(label: "com.example.BestExampleApp.main")
         logger.info("\(output)")
+
+        #if canImport(os)
+        os_log("This is a debug message.", log: OSLog.default, type: .debug)
+        #endif
     }
 }
