1f1f7e6239f41969c5e7ca272739d6704b1739a7
Chuck Grindel <chuck.grindel@gmail.com>
2024-01-15 10:59:28 -0700

    chore: upgrade vapor and fluent

--- examples/vapor_example/swift/Package.swift
+++ examples/vapor_example/swift/Package.swift
@@ -5,8 +5,8 @@ import PackageDescription
 let package = Package(
     name: "vapor_example",
     dependencies: [
-        .package(url: "https://github.com/vapor/vapor.git", exact: "4.89.3"),
-        .package(url: "https://github.com/vapor/fluent.git", exact: "4.8.0"),
+        .package(url: "https://github.com/vapor/vapor.git", exact: "4.91.1"),
+        .package(url: "https://github.com/vapor/fluent.git", exact: "4.9.0"),
         .package(url: "https://github.com/vapor/fluent-sqlite-driver.git", exact: "4.6.0"),
     ]
 )
