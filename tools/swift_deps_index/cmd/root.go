db31bd4293bdf924e8c664f6cb453fe235000e50
Chuck Grindel <chuck.grindel@gmail.com>
2024-04-28 12:47:33 -0600

    Add create command to swift_deps_index CLI.

--- tools/swift_deps_index/cmd/root.go
+++ tools/swift_deps_index/cmd/root.go
@@ -9,16 +9,8 @@ import (
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "swift_deps_index",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
+	Short: "Manage a Swift dependences index file.",
+	Long:  `Manage a Swift dependences index file.`,
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
