c68f5c8b947119396b2f63841328a32a71409a2c
John Flanagan <john.flanagan@doordash.com>
2024-05-06 10:28:35 -0500

    Add failing test.
    Compiling IterableSDK results in the following error:
    
    Error in fail: Expected only files inside directories named with the extensions ["xcdatamodel"], but found: [
      external/rules_swift_package_manager~~swift_deps~swiftpkg_swift_sdk/swift-sdk/Resources/IterableDataModel.xcdatamodeld
    ] datamodels

--- examples/resources_example/Tests/MyAppTests/MyAppTests.swift
+++ examples/resources_example/Tests/MyAppTests/MyAppTests.swift
@@ -1,5 +1,6 @@
 import AppLovinSDKResources
 import CoolUI
+import IterableSDK
 import SwiftUI
 import XCTest
 
@@ -12,5 +13,15 @@ class MyAppTests: XCTestCase {
     func test_AppLovinSDKResources() throws {
         let url = ALResourceManager.resourceBundleURL
         XCTAssertNotNil(url)
+    }
+    
+    func test_IterableDataModel() throws {
+        let dataModelLoaded = expectation("Iterable CoreData model loaded")
+        
+        let container = NSPersistentContainer(name: "IterableDataModel")
+        container.loadPersistentStores { _, error in
+            XCTAssertNil(error, "Loading persistence stores generated an error: \(error!)")
+            dataModelLoaded.fulfill()
+        }
     }
 }
