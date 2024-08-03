07cdd96477caa126797c57a4a883983034b1de9f
Chuck Grindel <chuck.grindel@gmail.com>
2024-08-03 12:10:25 -0600

    chore: catch up on stuck upgrades (#1190)

--- examples/stripe_example/Package.swift
+++ examples/stripe_example/Package.swift
@@ -7,7 +7,7 @@ let package = Package(
     dependencies: [
         .package(
             url: "https://github.com/stripe/stripe-ios",
-            from: "23.28.0"
+            from: "23.28.1"
         ),
     ]
 )
