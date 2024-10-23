c5dfb51b30c0f5c740dee6d3db9b85f2fa64bbf2
Tilak Shakya <shakyatilak45@gmail.com>
2024-10-23 19:36:49 +0530

    added single factor auth in shake ios example

--- examples/shake_ios_example/ShakeIOSExample/ContentView.swift
+++ examples/shake_ios_example/ShakeIOSExample/ContentView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import SingleFactorAuth
 
 struct ContentView: View {
     var body: some View {
@@ -16,6 +17,12 @@ struct ContentView: View {
             Text("Hello, world!")
         }
         .padding()
+        .onAppear {
+        let singleFactorAuthArgs = SingleFactorAuthArgs(
+        web3AuthClientId: "<Your Client Id>",
+        network: Web3AuthNetwork.SAPPHIRE_MAINNET)
+        let singleFactoreAuth = SingleFactorAuth(params: SFAParams)
+        }
     }
 }
 
--- examples/shake_ios_example/ShakeIOSExample/ShakeIOSExampleApp.swift
+++ examples/shake_ios_example/ShakeIOSExample/ShakeIOSExampleApp.swift
@@ -4,7 +4,7 @@ import SwiftUI
 @main
 struct ShakeIOSExampleApp: App {
     init() {
-        Shake.start(clientId: "_client_id_", clientSecret: "_client_secret_")
+        // Shake.start(clientId: "_client_id_", clientSecret: "_client_secret_")
     }
 
     var body: some Scene {
