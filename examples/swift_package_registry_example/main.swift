00897e2f759a402d49cb85c3bbaf919448c83cbf
Luis Padron <lpadron@squareup.com>
2024-10-29 21:54:43 -0400

    feat: add support for Swift package registries

---
+++ examples/swift_package_registry_example/main.swift
@@ -0,0 +1,10 @@
+import Collections
+import NIO
+
+let orderedSet = OrderedSet([1, 2, 3, 4, 5])
+
+print("Hello, world!")
+print(orderedSet)
+
+let buffer = ByteBuffer(string: "10101010101")
+print(buffer)
