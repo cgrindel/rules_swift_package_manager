952a4176bd08fd57f59e67bd1b1b8423a1baa9c3
Chuck Grindel <chuck@revealtech.ai>
2024-11-10 15:38:06 -0700

    Remvoe gazelle binary files.

--- gazelle/doc.go
+++
@@ -1,7 +0,0 @@
-// Package gazelle implements a [Bazel Gazelle] extension/plugin for the [Swift] programming
-// language. For information on how to use the plugin, please see the [Swift Bazel documentation].
-//
-// [Bazel Gazelle]: https://github.com/bazelbuild/bazel-gazelle
-// [Swift]: https://www.swift.org/
-// [Swift Bazel documentation]: https://github.com/cgrindel/rules_swift_package_manager
-package gazelle
