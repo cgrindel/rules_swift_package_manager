8927163105c924de37f2046b59532dceb8562de1
Logan Shire <logan.shire@gmail.com>
2025-01-05 17:21:54 -0300

    Tidy

--- examples/swift_protobuf_example/Package.swift
+++ examples/swift_protobuf_example/Package.swift
@@ -1,16 +1,15 @@
-// swift-tools-version: 5.8
+// swift-tools-version: 6.0
 
 import PackageDescription
 
 let package = Package(
-    name: "grpc_example",
+    name: "swift_protobuf_example",
     dependencies: [
         // These are the versions used by rules_swift
         // .package(url: "https://github.com/grpc/grpc-swift-extras", exact: "1.0.0-beta.2"),
         // .package(url: "https://github.com/grpc/grpc-swift.git", exact: "2.0.0-beta.2"), // Required by grpc-swift-extras @ 1.0.0-beta.2
         // .package(url: "https://github.com/grpc/grpc-swift-protobuf.git", exact: "1.0.0-beta.2"), // Required by grpc-swift @ 2.0.0-beta.2
         // .package(url: "https://github.com/grpc/grpc-swift-nio-transport.git", exact: "1.0.0-beta.2"), // Required by grpc-swift @ 2.0.0-beta.2
-        // .package(url: "https://github.com/apple/swift-protobuf.git", exact: "1.28.1"), // Required by grpc-swift @ 2.0.0-beta.2
-        .package(url: "https://github.com/AttilaTheFun/TestPackage.git", branch: "main")
+        .package(url: "https://github.com/apple/swift-protobuf.git", exact: "1.28.1"),
     ]
 )
