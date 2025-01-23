0b68a7888df8e7622f54c746baa661c59a499463
Lucas Marçal <127062836+0xLucasMarcal@users.noreply.github.com>
2025-01-23 11:56:59 -0300

    Update examples/pkg_manifest_minimal/Package.swift

--- examples/pkg_manifest_minimal/Package.swift
+++ examples/pkg_manifest_minimal/Package.swift
@@ -8,7 +8,6 @@ let package = Package(
         .package(url: "https://github.com/apple/swift-argument-parser", from: "1.5.0"),
         .package(url: "https://github.com/apple/swift-log", from: "1.6.2"),
         .package(url: "https://github.com/nicklockwood/SwiftFormat", from: "0.55.4"),
-
         .package(path: "third_party/my_local_package"),
         .package(path: "third_party/NotThatAmazingModule"),
     ]
