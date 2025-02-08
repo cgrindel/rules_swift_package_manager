d965f9099e2fd4194d46426fe30f97fe1f6cbc68
Chuck Grindel <chuck@revealtech.ai>
2025-02-08 10:37:26 -0700

    Remove System module from bzlmod example. Recent versions of Swift compiler error with circular dependency with Foundation.

--- bzlmod/workspace/Sources/MyExecutable/MyExecutable.swift
+++ bzlmod/workspace/Sources/MyExecutable/MyExecutable.swift
@@ -2,25 +2,22 @@ import ArgumentParser
 import Logging
 import MyLibrary
 import RegexBuilder
-import System
 
 // Because we build on macos/linux this needs to be conditionally imported to test an edge case
 #if canImport(os)
-import os
+    import os
 #endif
 
 @main
 struct MyExecutable: AsyncParsableCommand {
-    struct Plugin { }
+    struct Plugin {}
     mutating func run() async throws {
         let output = "Hello, \(World().name)!"
         let logger = Logger(label: "com.example.BestExampleApp.main")
         logger.info("\(output)")
 
         #if canImport(os)
-        os_log("This is a debug message.", log: OSLog.default, type: .debug)
+            os_log("This is a debug message.", log: OSLog.default, type: .debug)
         #endif
-
-        MySystem.hello()
     }
 }
