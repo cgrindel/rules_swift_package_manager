f00fe9cf7dcccac082792ca8c8ddefd1a99659e1
Logan Shire <logan.shire@gmail.com>
2025-03-27 10:49:19 -0400

    Use higher level grpc api and remove forked convenience wrappers

--- examples/grpc_example/sources/server/server_main.swift
+++ examples/grpc_example/sources/server/server_main.swift
@@ -12,7 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import Convenience
 import Foundation
 import GRPCCore
 import GRPCNIOTransportHTTP2
