fc9c77647ffa34b653659269efa099681b07b259
Adin Cebic <cebic.ad@gmail.com>
2025-05-08 18:27:45 +0200

    Address comments

--- examples/kscrash_example/Sources/MyApp/MyApp.swift
+++ examples/kscrash_example/Sources/MyApp/MyApp.swift
@@ -1,28 +1,26 @@
-import SwiftUI
 import KSCrashInstallations
+import SwiftUI
 
 @main
 struct MyApp: App {
-    var body: some Scene {
-        WindowGroup {
-            Text("Hello KSCrash")
-                .onAppear {
+  var body: some Scene {
+    WindowGroup {
+      Text("Hello KSCrash")
+        .onAppear {
+          let installation = CrashInstallationStandard.shared
+          installation.url = URL(string: "http://put.your.url.here")!
 
-                    let installation = CrashInstallationStandard.shared
-                    installation.url = URL(string: "http://put.your.url.here")!
+          let config = KSCrashConfiguration()
+          config.monitors = [.machException, .signal]
+          try! installation.install(with: config)
 
-                    let config = KSCrashConfiguration()
-                    config.monitors = [.machException, .signal]
-                    try! installation.install(with: config)
-
-                    installation.addConditionalAlert(
-                        withTitle: "Crash Detected",
-                        message: "The app crashed last time it was launched. Send a crash report?",
-                        yesAnswer: "Sure!",
-                        noAnswer: "No thanks"
-                    )
-
-                }
+          installation.addConditionalAlert(
+            withTitle: "Crash Detected",
+            message: "The app crashed last time it was launched. Send a crash report?",
+            yesAnswer: "Sure!",
+            noAnswer: "No thanks"
+          )
         }
     }
+  }
 }
