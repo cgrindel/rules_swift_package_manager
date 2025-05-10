6efbae0b55e96e58c06ce96eb14428d9c81060fc
Adin Cebic <cebic.ad@gmail.com>
2025-05-10 19:25:45 +0200

    Remove patch to KSCrash

--- examples/kscrash_example/swift/Package.swift
+++ examples/kscrash_example/swift/Package.swift
@@ -1,10 +1,10 @@
-// swift-tools-version: 5.7
+// swift-tools-version: 6.0
 
 import PackageDescription
 
 let package = Package(
     name: "resources_example",
     dependencies: [
-        .package(url: "https://github.com/kstenerud/KSCrash", exact: "2.1.0"),
+        .package(url: "https://github.com/kstenerud/KSCrash", exact: "2.1.1"),
     ]
 )
